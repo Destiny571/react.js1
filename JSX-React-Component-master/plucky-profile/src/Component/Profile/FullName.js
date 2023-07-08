ca7649004bb97a2dbb02e49cbf51f944be448413
Destiny571 <Ikperhadestiny50@gmail.com>
2023-07-08 13:29:53 +0100

    react checkpoint

---
+++ JSX-React-Component-master/plucky-profile/src/Component/Profile/FullName.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FullName = () => 
+{
+let firstName = "Ikperha";
+let middleName = "Pana";
+let lastName = "Destiny";
+
+return <p>{firstName + " " + middleName + " " + lastName}</p>;
+};
+
+export default FullName;
